6801828c60573215816a3c01213784faa69d0a23
joshi3 <joshi.naga3@gmail.com>
2017-10-10 16:51:17 +0530

    camera2 added

--- device.mk
+++ device.mk
@@ -94,7 +94,6 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/camera/camerasize.xml:system/etc/camerasize.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
@@ -310,3 +309,7 @@ PRODUCT_PACKAGES += \
 # Engineering mode
 PRODUCT_PACKAGES += \
 	EngineerMode
+
+#camera2
+PRODUCT_PACKAGES += \
+camera2
