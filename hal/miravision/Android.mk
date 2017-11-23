7c0e55f430b59fa2a77271c076bce2143b7d13c7
joshi3 <joshi.naga3@gmail.com>
2017-11-23 17:44:21 +0530

    miravision_jni to support mira vision

--- hal/miravision/Android.mk
+++ hal/miravision/Android.mk
@@ -6,16 +6,6 @@ LOCAL_SRC_FILES := \
 	miravision_jni.cpp
 	 
 LOCAL_C_INCLUDES := $(JNI_H_INCLUDE)
-
-LOCAL_C_INCLUDES += \
-        $(MTK_PATH_SOURCE)/hardware/dpframework/inc \
-        $(TOP)/bionic \
-        $(MTK_PATH_SOURCE)/kernel/include \
-        $(MTK_PATH_PLATFORM)/kernel/drivers/dispsys \
-        $(MTK_PATH_PLATFORM)/hardware/pq \
-        $(MTK_PATH_PLATFORM)/hardware/pq/inc \
-        $(MTK_PATH_PLATFORM)/hardware/aal/inc \
-
 	
 LOCAL_SHARED_LIBRARIES := \
 	libcutils \
