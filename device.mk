b3025ba114bd85bf7ef1d9049d4ab07ebd5869a6
joshi3 <joshi.naga3@gmail.com>
2017-12-04 23:30:11 +0530

    Fix sepolicy Ramdisk to make O compatible

--- device.mk
+++ device.mk
@@ -28,6 +28,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
 	Snap \
+	Camera2 \
 	libcamera_parameters_ext
 
 PRODUCT_PACKAGES += \
@@ -287,18 +288,15 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
 	gps.mt6795 \
+	android.hardware.gnss@1.0-impl \
 	libcurl
-	
+
 # Mediaserver with system group
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/etc/init/mediaserver.rc:system/etc/init/mediaserver.rc
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/etc/init/cameraserver.rc:system/etc/init/cameraserver.rc
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/etc/init/drmserver.rc:system/etc/init/drmserver.rc
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/etc/init/mediacodec.rc:system/etc/init/mediacodec.rc
-PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/etc/init/mediaserver.rc:system/etc/init/mediaserver.rc \
+	$(LOCAL_PATH)/etc/init/cameraserver.rc:system/etc/init/cameraserver.rc \
+	$(LOCAL_PATH)/etc/init/drmserver.rc:system/etc/init/drmserver.rc \
+	$(LOCAL_PATH)/etc/init/mediacodec.rc:system/etc/init/mediacodec.rc \
 	$(LOCAL_PATH)/etc/init/mediadrmserver.rc:system/etc/init/mediadrmserver.rc
 
 # camera legacy
@@ -334,3 +332,65 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.service.adb.enable=1 \
     persist.service.debuggable=1
+
+# Audio HAL
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@2.0-service
+#    android.hardware.soundtrigger@2.0-impl \
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
+
+# Camera HAL
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service
+
+# Graphic HAL
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
+
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
+# Memtrack HAL
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
+
+#USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+PRODUCT_PACKAGES += \
+    PerformanceControl \
+    Stk
+
+PRODUCT_PROPERTY_OVERRIDES += \
+   persist.force.gps.mode=gnss
+
